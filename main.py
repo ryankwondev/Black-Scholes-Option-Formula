dffc10acd27c7535aa97ac058d557b4a7816eff7
Ryan Kwon <kznm.develop@gmail.com>
2023-05-13 21:07:41 +0900

    production edit

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ import numpy as np
 import scipy.stats as stat
 import plotly.graph_objs as go
 
+# ========== FOR PRODUCTION USE ==========
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+# ========================================
+
+st.title('Black-Scholes Option Pricing Model')
+st.write('This is a simple example of using Black-Scholes Option Pricing Model')
+st.write('For MO-BSRA Forum, by @ryankwondev')
 
 # Black-Scholes Option Formula
 def europian_option(S, K, T, r, sigma, option_type):
@@ -31,8 +44,20 @@ r = st.sidebar.number_input('Risk Free Rate', value=r)
 sigma = st.sidebar.number_input('Volatility', value=sigma)
 
 # Variables
-T = np.linspace(1e-10, 8, 100)
-S = np.linspace(179000, 242200, 100)
+# T = np.linspace(1e-10, 8, 100)
+# S = np.linspace(179000, 242200, 100)
+# T, S = np.meshgrid(T, S)
+
+# T/S Param
+st.sidebar.header('T/S Parameters')
+T_min = st.sidebar.number_input('T min', value=1e-10)
+T_max = st.sidebar.number_input('T max', value=8)
+S_min = st.sidebar.number_input('S min', value=179000)
+S_max = st.sidebar.number_input('S max', value=242200)
+
+# T/S Range
+T = np.linspace(T_min, T_max, 100)
+S = np.linspace(S_min, S_max, 100)
 T, S = np.meshgrid(T, S)
 
 # Output
